08eb18a17614a138dbbce5ed00fa28440d287731
Jonny Klemmer <jklemmer@clientresourcesinc.com>
2018-12-31 12:32:13 -0600

    Initial Commit

---
+++ TopAlbums/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  TopAlbums
+//
+//  Created by Jonny Klemmer on 12/31/18.
+//  Copyright © 2018 Jonny Klemmer. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
