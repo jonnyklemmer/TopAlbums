f321b4fcca17028aa9af2760445d3faa7ddb702d
Jonny Klemmer <jklemmer@clientresourcesinc.com>
2019-01-01 16:20:34 -0500

    Add unit tests to new setup.

--- TopAlbumsTests/TopAlbumsTests.swift
+++ TopAlbumsTests/TopAlbumsTests.swift
@@ -10,25 +10,41 @@ import XCTest
 @testable import TopAlbums
 
 class TopAlbumsTests: XCTestCase {
+    
+    var viewModel: AlbumsViewModel!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let albumVM = AlbumViewModel(withAlbum: Album(artistName: "artistName",
+                                                      id: "id",
+                                                      releaseDate: "releaseDate",
+                                                      name: "name",
+                                                      copyright: "copyright",
+                                                      genres: [Genre(genreId: "genreId", name: "genre-name")], artworkUrl100: "artworkUrl100", url: "url"))
+        viewModel = AlbumsViewModel(albumViewModels: [albumVM])
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAlbumInitializedSuccessfully() {
+        XCTAssertTrue(viewModel.numberOfAlbums > 0)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAlbumDataParsedSuccessfully() {
+        guard let albumVM = viewModel.viewModel(atIndex: 0) else {
+            XCTFail()
+            return
         }
+        
+        XCTAssertEqual(albumVM.artistName, "artistName")
+        XCTAssertEqual(albumVM.releaseDate, "releaseDate")
+        XCTAssertEqual(albumVM.name, "name")
+        XCTAssertEqual(albumVM.copyright, "copyright")
+        
+        XCTAssertEqual(albumVM.genres, ["genre-name"])
+        XCTAssertEqual(albumVM.imageUrl, "artworkUrl100")
     }
 
 }
